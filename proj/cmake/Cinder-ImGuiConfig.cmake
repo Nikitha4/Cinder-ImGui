5831491a9ceeb406e5f1b9e0a36a50ea95f82aa5
Nikitha Nagumalli <nikitha4@illinois.edu>
2020-04-30 14:18:29 -0500

    started adding Cinder-ImGui for final project, still need to do the linking process

--- proj/cmake/Cinder-ImGuiConfig.cmake
+++ proj/cmake/Cinder-ImGuiConfig.cmake
@@ -1,29 +1,46 @@
-if( NOT TARGET Cinder-ImGui )
-  get_filename_component( IMGUI_ROOT_PATH "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE )
+if(NOT TARGET Cinder-Notifications)
+	# Define ${Cinder-Notifications_PROJECT_ROOT}. ${CMAKE_CURRENT_LIST_DIR} is just the current directory.
+	get_filename_component(Cinder-Notifications_PROJECT_ROOT "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE)
 
-  list( APPEND IMGUI_SOURCES
-	${IMGUI_ROOT_PATH}/src/CinderImGui.cpp
-	${IMGUI_ROOT_PATH}/lib/imgui/imgui.cpp
-	${IMGUI_ROOT_PATH}/lib/imgui/imgui_draw.cpp
-	${IMGUI_ROOT_PATH}/lib/imgui/imgui_widgets.cpp
-	)
+	# Define ${CINDER_PATH} as usual.
+	get_filename_component(CINDER_PATH "${Cinder-Notifications_PROJECT_ROOT}/../.." ABSOLUTE)
 
-  add_library( Cinder-ImGui ${IMGUI_SOURCES} )
+	# Make a list of source files and define that to be ${SOURCE_LIST}.
+	file(GLOB SOURCE_LIST CONFIGURE_DEPENDS
+			"${Cinder-Notifications_PROJECT_ROOT}/src/rph/NotificationManager.cpp"
+			"${Cinder-Notifications_PROJECT_ROOT}/src/rph/Notification.cpp"
+			"${Cinder-Notifications_PROJECT_ROOT}/src/**/*.h"
+			"${Cinder-Notifications_PROJECT_ROOT}/src/**/*.hpp"
+			"${Cinder-Notifications_PROJECT_ROOT}/src/**/*.cc"
+			"${Cinder-Notifications_PROJECT_ROOT}/src/**/*.cpp"
+			"${Cinder-Notifications_PROJECT_ROOT}/src/**/*.c"
+			"${Cinder-Notifications_PROJECT_ROOT}/src/*.h"
+			"${Cinder-Notifications_PROJECT_ROOT}/src/*.hpp"
+			"${Cinder-Notifications_PROJECT_ROOT}/src/*.cc"
+			"${Cinder-Notifications_PROJECT_ROOT}/src/*.cpp"
+			"${Cinder-Notifications_PROJECT_ROOT}/src/*.c")
 
-  list( APPEND IMGUI_INCLUDE_DIRS
-	${IMGUI_ROOT_PATH}/include
-	${IMGUI_ROOT_PATH}/lib/imgui
-	)
-  
-  target_include_directories( Cinder-ImGui PUBLIC "${IMGUI_INCLUDE_DIRS}" )
+	# Create the library!
+	add_library(Cinder-Notifications ${SOURCE_LIST})
 
-  if( NOT TARGET cinder )
-	include( "${CINDER_PATH}/proj/cmake/configure.cmake" )
-	find_package( cinder REQUIRED PATHS
-	  "${CINDER_PATH}/${CINDER_LIB_DIRECTORY}"
-	  "$ENV{CINDER_PATH}/${CINDER_LIB_DIRECTORY}" )
-  endif()
-  
-  target_link_libraries( Cinder-ImGui PRIVATE cinder )
-  
+	# Add include directories.
+	# Notice that `cinderblock.xml` has `<includePath>src</includePath>`.
+	# So you need to set `../../src/` to include.
+	target_include_directories(Cinder-Notifications PUBLIC "${Cinder-Notifications_PROJECT_ROOT}/src" )
+	target_include_directories(Cinder-Notifications SYSTEM BEFORE PUBLIC "${CINDER_PATH}/include" )
+
+
+	# If your Cinder block has no source code but instead pre-build libraries,
+	# you can specify all of them here (uncomment the below line and adjust to your needs).
+	# Make sure to use the libraries for the right platform.
+	# # target_link_libraries(Cinder-Notifications "${Cinder-OpenCV_PROJECT_ROOT}/lib/libopencv_core.a")
+
+	if(NOT TARGET cinder)
+		include("${CINDER_PATH}/proj/cmake/configure.cmake")
+		find_package(cinder REQUIRED PATHS
+				"${CINDER_PATH}/${CINDER_LIB_DIRECTORY}"
+				"$ENV{CINDER_PATH}/${CINDER_LIB_DIRECTORY}")
+	endif()
+	target_link_libraries(Cinder-Notifications PRIVATE cinder)
+
 endif()
